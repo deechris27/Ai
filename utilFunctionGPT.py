2d60acd7fdd4311043668e2fda1a8884818ddef8
deepak <dee@gmail.com>
2025-04-22 00:26:31 +0530

    cleanup

--- utilFunctionGPT.py
+++ utilFunctionGPT.py
@@ -4,7 +4,6 @@ import os
 import requests
 from dotenv import load_dotenv
 from openai import OpenAI
-from IPython.display import Markdown, display, update_display
 import time
 
 load_dotenv(override=True)
