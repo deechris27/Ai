33ae50a97fd898af453f497f77f5ac503fda503a
deepak <dee@gmail.com>
2025-04-22 13:20:03 +0530

    feat: google gemini through openai api client

--- googleGemini.py
+++ googleGemini.py
@@ -2,10 +2,11 @@ import google.generativeai
 import os
 import requests
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv(override=True)
 gemini_api_key = os.getenv('GOOGLE_API_KEY')
-
+openai = OpenAI()
 
 if not gemini_api_key:
     print('No API key found')
@@ -25,4 +26,19 @@ gemini = google.generativeai.GenerativeModel(
 
 response = gemini.generate_content(user_prompt)
 
-print(response.text)
+#print(response.text)
+
+gemini_via_openapi_client = OpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = gemini_via_openapi_client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages= [
+        {"role": "system", "content": "You are an astro scientist"},
+        {"role": "user", "content": "Do you think Humans will colonize another planet someday?"}
+    ]
+)
+
+print(response.choices[0].message.content)
